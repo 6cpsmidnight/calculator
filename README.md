6c6cca45b5a8388d85ecda9498f6f1c929cff079
Cyber Coder <cyberelectro101@gmail.com>
2020-11-20 22:51:41 -0500

    Initial calculator design.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# calculator
-A digital calculator for math because my dad is hogging the one we bought at the start of the school year.
+# A digital calculator for math
+because my dad is hogging the one we bought at the start of the school year.
---
+++ external-code/index.js
@@ -0,0 +1,24 @@
+// let calculation = "";
+
+// // Buttons - Keystrokes
+// $(document).keydown(function (event) {
+
+//     switch (event.key) {
+
+//         case ("1"):
+
+//             calculation += 1;
+
+//             break;
+
+//         default:
+
+//             break;
+//     }
+// })
+
+// // Buttons - Delete
+// $()
+
+// // Title - Loading
+// document.title = "Calculator";
