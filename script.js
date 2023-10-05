de9190d37e70d49b11232056b86e4986baa8c795
6cpsmidnight <cyberelectro101@gmail.com>
2023-10-05 19:06:43 -0400

    Ability to use keyboard to input...
    + visual bug with math constants dropdown fixed
    + more visual bugs fixed

--- script.js
+++ script.js
@@ -4,20 +4,174 @@ let operatorId = 0;
 let operator = "";
 let answer = 0;
 
-const calc = document.getElementById("calculator");
+const calcEl = document.getElementById("calculator");
 
 const tCalcDisp = document.getElementById("topCalcDisp");
 const bCalcDisp = document.getElementById("bottomCalcDisp");
 
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");
-const del = document.getElementById("del");
-const clr = document.getElementById("clr");
-const output = document.getElementById("output");
+const allNumBtn = document.querySelectorAll(".btn-num");
+const decimalBtn = document.getElementById("decimalBtn");
+const plusBtn = document.getElementById("plusBtn");
+const minusBtn = document.getElementById("minusBtn");
+const multiplyBtn = document.getElementById("multiplyBtn");
+const divideBtn = document.getElementById("divideBtn");
+const delBtn = document.getElementById("delBtn");
+const clrBtn = document.getElementById("clrBtn");
+const outputBtn = document.getElementById("outputBtn");
 
+const mathConstantsBtn = document.getElementById("mathConst");
 const mathConstantsDropdown = document.getElementById("mathConstantsDropdown");
+const mathConstsDropdownSummary = document.querySelector("#mathConstantsDropdown>summary");
+const mathConstsDropdownChildDiv = document.querySelector("#mathConstantsDropdown>div");
+
+function calculate(e) {
+    if (e <= 9) {
+        if (operatorId === 0) {
+            origTxtClr();
+            clrOutcome()
+
+            firstIn = rmvUnccsryZrosOnHead(firstIn);
+            firstIn += e.toString();
+            bCalcDisp.innerHTML = firstIn;
+
+            toggleOperators(1);
+            toggleBtn(clrBtn, 1);
+        } else {
+            if (secondIn.length === 0) {
+                tCalcDisp.innerHTML = firstIn + operator;
+
+                toggleOperators(0);
+                toggleBtn(outputBtn, 1);
+            }
+
+            secondIn += e.toString();
+            bCalcDisp.innerHTML = secondIn;
+        }
+
+        toggleBtn(delBtn, 1);
+    } else if (e === 10) {
+        if (operatorId === 0) {
+            origTxtClr();
+            clrOutcome()
+
+            firstIn += ".";
+            bCalcDisp.innerHTML = firstIn;
+
+            toggleOperators(1);
+            toggleBtn(delBtn, 1);
+            toggleBtn(clrBtn, 1);
+
+            btnClickAnim(decimalBtn);
+        } else {
+            if (secondIn.length === 0) {
+                tCalcDisp.innerHTML += operator;
+
+                toggleOperators(0);
+                toggleBtn(outputBtn, 1);
+            }
+
+            secondIn += ".";
+            bCalcDisp.innerHTML = secondIn;
+            btnClickAnim(decimalBtn);
+        }
+    } else if (firstIn.length > 0 && secondIn.length === 0 && e >= 11 && e <= 14) {
+        firstIn = rmvUnccsryZrosAndDecsOnTail(firstIn);
+        tCalcDisp.innerHTML = firstIn;
+
+        optrChg(e);
+        bCalcDisp.innerHTML = operator;
+    } else if (e === 15) {
+        if (tCalcDisp.innerHTML !== "Made by" && bCalcDisp.length !== 0 && firstIn.length !== 0) {
+            btnClickAnim(delBtn);
+
+            if (operatorId === 0) {
+                firstIn = firstIn.slice(0, -1);
+                bCalcDisp.innerHTML = firstIn;
+
+                if (firstIn.length === 0) {
+                    toggleOperators(0);
+                    toggleBtn(delBtn, 0);
+                    toggleBtn(clrBtn, 0);
+                    toggleBtn(outputBtn, 0);
+                }
+            } else if (operatorId !== 0 && secondIn.length === 0) {
+                optrChg(0);
+
+                tCalcDisp.innerHTML = "‎ ";
+                bCalcDisp.innerHTML = firstIn;
+            } else if (secondIn.length !== 0) {
+                secondIn = secondIn.slice(0, -1);
+                bCalcDisp.innerHTML = secondIn;
+
+                if (secondIn.length === 0) {
+                    tCalcDisp.innerHTML = firstIn;
+                    bCalcDisp.innerHTML = operator;
+
+                    toggleOperators(1);
+                    toggleBtn(outputBtn, 0);
+                }
+            }
+        }
+    } else if (e === 16) {
+        mathConstantsDropdown.setAttribute("open", "true");
+        mathConstantsDropdown.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    } else if (e === 17) {
+        if (bCalcDisp.innerHTML.length !== 0 && bCalcDisp.innerHTML !== "‎ ") {
+            clrCalcDisp();
+
+            toggleOperators(0);
+            toggleBtn(delBtn, 0);
+            toggleBtn(clrBtn, 0);
+            toggleBtn(outputBtn, 0);
+
+            clrData();
+
+            btnClickAnim(clrBtn);
+        }
+    } else if (e === 18) {
+        if (secondIn.length !== 0) {
+            secondIn = rmvUnccsryZrosAndDecsOnTail(secondIn);
+
+            tCalcDisp.innerHTML = firstIn + operator + secondIn + "=";
+
+            switch (operatorId) {
+                case 1:
+                    answer = Number(firstIn) + Number(secondIn);
+                    break;
+                case 2:
+                    answer = Number(firstIn) - Number(secondIn);
+                    break;
+                case 3:
+                    answer = Number(firstIn) * Number(secondIn);
+                    break;
+                case 4:
+                    answer = Number(firstIn) / Number(secondIn);
+                    break;
+                default:
+                    answer = "Err";
+            }
+
+            if (isNaN(answer)) {
+                answer = "Err";
+            }
+
+            bCalcDisp.innerHTML = answer;
+            bCalcDisp.classList.add("scale-animation");
+            sleep(100).then(() => {
+                bCalcDisp.classList.remove("scale-animation");
+            });
+            btnClickAnim(outputBtn);
+
+            toggleBtn(delBtn, 0);
+            toggleBtn(outputBtn, 0);
+
+            clrData();
+        }
+    }
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -38,22 +192,22 @@ function optrChg(optr) {
         case 11:
             operatorId = 1;
             operator = "+";
-            btnClickAnim(plus);
+            btnClickAnim(plusBtn);
             break;
         case 12:
             operatorId = 2;
             operator = "-";
-            btnClickAnim(minus);
+            btnClickAnim(minusBtn);
             break;
         case 13:
             operatorId = 3;
             operator = "*";
-            btnClickAnim(multiply);
+            btnClickAnim(multiplyBtn);
             break;
         case 14:
             operatorId = 4;
             operator = "/";
-            btnClickAnim(divide);
+            btnClickAnim(divideBtn);
             break;
     }
 }
@@ -85,7 +239,8 @@ function rmvUnccsryZrosAndDecsOnTail(i) {
             if (i === ".") {
                 i = "0";
                 return i;
-            } if (i.length === 1) {
+            }
+            if (i.length === 1) {
                 return i;
             } else {
                 i = i.slice(0, -1);
@@ -128,183 +283,79 @@ function btnClickAnim(btn) {
 function toggleOperators(o) {
     switch (o) {
         case 0:
-            toggleBtn(plus, 0);
-            toggleBtn(minus, 0);
-            toggleBtn(multiply, 0);
-            toggleBtn(divide, 0);
+            toggleBtn(plusBtn, 0);
+            toggleBtn(minusBtn, 0);
+            toggleBtn(multiplyBtn, 0);
+            toggleBtn(divideBtn, 0);
             break;
         case 1:
-            toggleBtn(plus, 1);
-            toggleBtn(minus, 1);
-            toggleBtn(multiply, 1);
-            toggleBtn(divide, 1);
+            toggleBtn(plusBtn, 1);
+            toggleBtn(minusBtn, 1);
+            toggleBtn(multiplyBtn, 1);
+            toggleBtn(divideBtn, 1);
             break;
     }
 }
 
 toggleOperators(0);
-toggleBtn(del, 0);
-toggleBtn(output, 0);
+toggleBtn(delBtn, 0);
+toggleBtn(outputBtn, 0);
 
-function input(e) {
-    if (e <= 9) {
-        if (operatorId === 0) {
-            origTxtClr();
-            clrOutcome()
-
-            firstIn = rmvUnccsryZrosOnHead(firstIn);
-            firstIn += e.toString();
-            bCalcDisp.innerHTML = firstIn;
-
-            toggleOperators(1);
-            toggleBtn(clr, 1);
-        } else {
-            if (secondIn.length === 0) {
-                tCalcDisp.innerHTML = firstIn + operator;
-
-                toggleOperators(0);
-                toggleBtn(output, 1);
-            }
-
-            secondIn += e.toString();
-            bCalcDisp.innerHTML = secondIn;
-        }
-
-        toggleBtn(del, 1);
-    } else if (e === 10) {
-        if (operatorId === 0) {
-            origTxtClr();
-            clrOutcome()
-
-            firstIn += ".";
-            bCalcDisp.innerHTML = firstIn;
-
-            toggleOperators(1);
-            toggleBtn(del, 1);
-            toggleBtn(clr, 1);
-        } else {
-            if (secondIn.length === 0) {
-                tCalcDisp.innerHTML += operator;
-
-                toggleOperators(0);
-                toggleBtn(output, 1);
-            }
-
-            secondIn += ".";
-            bCalcDisp.innerHTML = secondIn;
-        }
-    } else if (firstIn.length > 0 && secondIn.length === 0 && e >= 11 && e <= 14) {
-        firstIn = rmvUnccsryZrosAndDecsOnTail(firstIn);
-        tCalcDisp.innerHTML = firstIn;
-
-        optrChg(e);
-        bCalcDisp.innerHTML = operator;
-    } else if (e === 15) {
-        if (tCalcDisp.innerHTML !== "Made by" && bCalcDisp.length !== 0 && firstIn.length !== 0) {
-            btnClickAnim(del);
-
-            if (operatorId === 0) {
-                firstIn = firstIn.slice(0, -1);
-                bCalcDisp.innerHTML = firstIn;
-
-                if (firstIn.length === 0) {
-                    toggleOperators(0);
-                    toggleBtn(del, 0);
-                    toggleBtn(clr, 0);
-                    toggleBtn(output, 0);
-                }
-            } else if (operatorId !== 0 && secondIn.length === 0) {
-                optrChg(0);
-
-                tCalcDisp.innerHTML = "‎ ";
-                bCalcDisp.innerHTML = firstIn;
-            } else if (secondIn.length !== 0) {
-                secondIn = secondIn.slice(0, -1);
-                bCalcDisp.innerHTML = secondIn;
-
-                if (secondIn.length === 0) {
-                    tCalcDisp.innerHTML = firstIn;
-                    bCalcDisp.innerHTML = operator;
-
-                    toggleOperators(1);
-                    toggleBtn(output, 0);
-                }
-            }
-        }
-    } else if (e === 16) {
-        mathConstantsDropdown.setAttribute("open", "true");
-        mathConstantsDropdown.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    } else if (e === 17) {
-        if (bCalcDisp.innerHTML.length !== 0) {
-            clrCalcDisp();
-
-            toggleOperators(0);
-            toggleBtn(del, 0);
-            toggleBtn(clr, 0);
-
-            clrData();
-
-            btnClickAnim(clr);
-        }
-    } else if (e === 18) {
-        if (secondIn.length !== 0) {
-            secondIn = rmvUnccsryZrosAndDecsOnTail(secondIn);
-
-            tCalcDisp.innerHTML = firstIn + operator + secondIn + "=";
-
-            switch (operatorId) {
-                case 1:
-                    answer = Number(firstIn) + Number(secondIn);
-                    break;
-                case 2:
-                    answer = Number(firstIn) - Number(secondIn);
-                    break;
-                case 3:
-                    answer = Number(firstIn) * Number(secondIn);
-                    break;
-                case 4:
-                    answer = Number(firstIn) / Number(secondIn);
-                    break;
-                default:
-                    answer = "Err";
-            }
-
-            if (isNaN(answer)) {
-                answer = "Err";
-            }
-
-            bCalcDisp.innerHTML = answer;
-            bCalcDisp.classList.add("scale-animation");
-            sleep(100).then(() => {
-                bCalcDisp.classList.remove("scale-animation");
-            });
-
-            toggleBtn(del, 0);
-            toggleBtn(output, 0);
-
-            clrData();
-        }
-    }
-}
-
-let calculatorYPosition = calc.getBoundingClientRect().top;
-let calculatorHeight = calc.offsetHeight;
-
-let setMCYResizeTimer;
+let calculatorYPosition = calcEl.getBoundingClientRect().top;
+let calculatorHeight = calcEl.offsetHeight;
+let mathConstsDropdownSummaryHeight = mathConstsDropdownSummary.offsetHeight;
 
 function setMathConstY() {
-    calculatorYPosition = calc.getBoundingClientRect().top;
-    calculatorHeight = calc.offsetHeight;
+    calculatorYPosition = calcEl.getBoundingClientRect().top;
+    calculatorHeight = calcEl.offsetHeight;
 
     mathConstantsDropdown.style.top = (calculatorYPosition + calculatorHeight + 25) + "px";
+    mathConstsDropdownChildDiv.style.top = mathConstsDropdownSummaryHeight + "px";
 }
 
 setMathConstY();
 
-window.addEventListener("resize", function () {
-    clearTimeout(setMCYResizeTimer);
-    resizeTimer = setTimeout(setMathConstY, 200)
+let numKeyInput;
+
+document.addEventListener("keydown", function (e) {
+    if (Number(e.key) <= 10) {
+        numKeyInput = Number(e.key);
+
+        calculate(numKeyInput);
+
+        for (let i = 0; i < 10; i++) {
+            if (Number(allNumBtn[i].textContent) === numKeyInput) {
+                btnClickAnim(allNumBtn[i]);
+            }
+        }
+    } else if (e.key === "Backspace") {
+        calculate(15);
+    } else if (e.key === "Delete") {
+        calculate(17);
+    } else if (e.key === "Enter") {
+        calculate(18);
+    } else {
+        switch (e.key) {
+            case ".":
+                calculate(10);
+                break;
+            case "+":
+                calculate(11);
+                break;
+            case "-":
+                calculate(12);
+                break;
+            case "*":
+                calculate(13);
+                break;
+            case "/":
+                calculate(14);
+                break;
+
+        }
+    }
+    if (e.keyCode === 13) {
+        // The "Enter" key was pressed
+        console.log("You pressed the Enter key.");
+    }
 });
